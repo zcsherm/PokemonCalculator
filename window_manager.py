f8727d7a309aee8609d0ca24fd1b9a6120856d85
zcsherm <165740629+zcsherm@users.noreply.github.com>
2025-07-30 16:18:28 -0700

    Update window_manager.py

--- window_manager.py
+++ window_manager.py
@@ -1,3 +1,4 @@
+import main_page
 import pickle as pkl
 import tkinter as tk
 from tkinter import messagebox
@@ -6,5 +7,17 @@ class Launcher:
     def __init__(self):
         self._root = tk.Tk()
         self.root.withdraw()
-        #self._window = login_screen.LoginScreen(master=self)
-        self._current = self._window
+        self.launch_home()
+
+
+    def launch_home(self, source=None):
+        self._current = main_page.MainPage(master=self)
+        if source:
+            source.destroy()
+        self._current.force_focus()
+        
+    def handle_options(self, source, options):
+        """
+        Handles requests for new pages from the Home Screen
+        """
+        pass
