4e573287c5eb880f77a63fa92c9846e85bbbcfc1
zcsherm <165740629+zcsherm@users.noreply.github.com>
2025-07-30 14:56:18 -0700

    Update window_manager.py

--- window_manager.py
+++ window_manager.py
@@ -1,3 +1,10 @@
+import pickle as pkl
+import tkinter as tk
+from tkinter import messagebox
+
 class Launcher:
     def __init__(self):
-        pass
+        self._root = tk.Tk()
+        self.root.withdraw()
+        #self._window = login_screen.LoginScreen(master=self)
+        self._current = self._window
