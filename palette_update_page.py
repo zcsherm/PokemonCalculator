b53c9034dc2285e3c4f9389983ebf8f766c62242
zcsherm <165740629+zcsherm@users.noreply.github.com>
2025-07-31 13:23:22 -0700

    Update palette_update_page.py

--- palette_update_page.py
+++ palette_update_page.py
@@ -1,1 +1,25 @@
+import math
+import os.path
+import pickle
+import sys
+from tkinter import messagebox, ttk
+import tkinter as tk
 
+class PaletteUpdatePage(tk.Toplevel):
+    def __init__(self, master, mon, palette_type= "normal):
+        super().__init__()
+        self._master = master
+        self._mon = mon
+        self.title("Update Palette Data")
+        self.focus_set()
+        self.calc = Calculator(mon, self)              # Need to reformat 
+
+        # Create frames
+        frame1 = tk.LabelFrame(self, borderwidth=1)    # For the images
+        frame1.grid(row=0,column=0)
+
+        frame2 = tk.LabelFrame(self, borderwidth=1)    # For the tools
+        frame2.grid(row=0, column=1)
+
+        frame3 = tk.LabelFrame(self, borderwidth=1)    # For action buttons?
+        frame3.grid(row=0, column=2)
